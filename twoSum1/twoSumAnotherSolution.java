4e0b0a74fce18755b1cfa660e25ed031e90e4797
Amine Lakhal <96929412+aminelkl@users.noreply.github.com>
2022-07-20 22:52:22 -0400

    Create twoSumAnotherSolution.java

---
+++ twoSum1/twoSumAnotherSolution.java
@@ -0,0 +1,26 @@
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+*/
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        int[] answer = new int[2];
+        
+  for(int i=0; i<nums.length; i++){
+            for(int j=i+1; j<nums.length; j++){
+                int reste = target - nums[i]; 
+                if (nums[j]==reste){
+                    answer[0] = i;
+                    answer[1] = j;
+                }
+                     
+            }
+        }
+    return answer;
+    }
+}
